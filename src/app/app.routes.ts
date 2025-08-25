60bdda521712369d3e2bd802561ed3abc1796fbf
Olga Biro <ola@biro.family>
2025-08-25 22:33:39 +0200

    Add main menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {CompletionHelp} from './completion-help/completion-help';
+import {DishList} from './dish/dish-list/dish-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CompletionHelp },
+  { path: 'completion', component: CompletionHelp },
+  { path: 'dishes', component: DishList },
+  { path: 'ingredients', component: DishList }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
 import {CompletionHelp} from './completion-help/completion-help';
+import {Header} from './header/header';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CompletionHelp],
+  imports: [CompletionHelp, Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
